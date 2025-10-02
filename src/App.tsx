75d02dfee0998d6421d739733380f8556a7114c0
Fernanado Nina <favio5449@gmail.com>
2025-10-02 12:31:11 -0600

    feat: llamado de componentes de una API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface FotosType {
+  id: number;
+  author: string;
+  url: string;
+  download_url: string;
 }
 
-export default App
+export default function App() {
+  const [fotos, setFotos] = useState<FotosType[]>([]);
+  const obtenerDatos = async () => {
+    try {
+      const result = await fetch(
+        'https://picsum.photos/v2/list?page=2&limit=25'
+      ).then((res) => res.json());
+      setFotos(result);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
+  return (
+    <div className="container-fotos">
+      <h1>Galeria de Imagenes</h1>
+      <div
+      style={{
+        display: 'flex',
+        padding: '20px',
+        gridTemplateColumns: 'repeat(4, 1fr)',
+        gap: '20px',
+        justifyContent: 'center',
+      }}
+    >
+      
+      {fotos.map((foto) => (
+        <>
+          
+          <img src={foto.download_url}
+          style={{
+             
+              width: '200px',
+              height: '200px',
+              borderRadius: '8px',
+              boxShadow: '0 4px 8px',
+              marginBottom: '16px',
+            }} />
+            <p> Autor: {foto.author} </p>
+        
+        </>
+      ))}
+    </div>
+    </div>
+  );
+}
